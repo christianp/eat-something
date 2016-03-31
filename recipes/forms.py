5d831c3bdc3e4e412ab8aa0b617fbad9800f67dc
Christian Lawson-Perfect <christianperfect@gmail.com>
2016-03-31 20:36:37 +0100

    time field takes readable values

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,6 +1,9 @@
 from .models import Meal, Recipe
 from django.forms import ModelForm, inlineformset_factory
 from django import forms
+from django.utils import dateparse
+import re
+import datetime
 
 class CreateRecipe(ModelForm):
     class Meta:
@@ -15,7 +18,49 @@ class Meal(ModelForm):
             'category': forms.CheckboxSelectMultiple(),
         }
 
+english_duration_re = re.compile(r'\s*(?:,|and)?\s*(?P<amount>\d+) (?P<unit>day|d|hour|h|minute|min|m|second|s)s?')
+english_duration_all_re = re.compile('^(?:{})+$'.format(english_duration_re.pattern))
+time_unit_translation = {
+    'day': 'day',
+    'd': 'day',
+    'hour': 'hour',
+    'h': 'hour',
+    'minute': 'minute',
+    'min': 'minute',
+    'm': 'minute',
+    'second': 'second',
+    's': 'second',
+}
+
+def parse_duration(value):
+    value = value.strip()
+    if english_duration_all_re.match(value):
+        days = 0
+        seconds = 0
+        d = {}
+        for m in english_duration_re.finditer(value):
+            unit = m.group('unit')
+            unit = time_unit_translation[unit]
+            amount = m.group('amount')
+            d[unit+'s'] = float(amount)
+        return datetime.timedelta(**d)
+    else:
+        return dateparse.parse_duration(value)
+
+
+class CleverDurationField(forms.DurationField):
+    def to_python(self,value):
+        if value in self.empty_values:
+            return None
+        if isinstance(value, datetime.timedelta):
+            return value
+        value = parse_duration(value)
+        if value is None:
+            raise ValidationError(self.error_messages['invalid'], code='invalid')
+        return value
+
 class Recipe(ModelForm):
+    time = CleverDurationField()
     class Meta:
         model = Recipe
         fields = ('meal','url','reference','ingredients','instructions','time')
